2f19e3ba474e6cd8eb5b377d669ec1c935203bb8
LeoAvMo <avilamolinaleonardo@gmail.com>
2026-05-11 17:10:28 +0200

    Initial commit

---
+++ BezierCurvesPractice/BezierCurvesPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BezierCurvesPracticeApp.swift
+//  BezierCurvesPractice
+//
+//  Created by Leo A.Molina on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BezierCurvesPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
